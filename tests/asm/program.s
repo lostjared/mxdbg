079fe5535849ffa0db01bb1e4b2b8ad672f6664d
Jared Bruni <jaredbruni@gmail.com>
2025-07-02 15:25:19 -0700

    updated asm test

--- tests/asm/program.s
+++ tests/asm/program.s
@@ -1,6 +1,6 @@
 .section .data
     hello_msg: .asciz "Hello World"
-    print_digits: .asciz "The number: %d\n"
+    print_digits: .asciz "The number: %d %d\n"
 
 .section .text
     .global main
@@ -8,19 +8,29 @@
 main:
     push %rbp           
     mov %rsp, %rbp      
-    
+
+    sub $16, %rsp
+    movq $0, -0x8(%rbp)
+
     lea hello_msg(%rip), %rdi    # First argument for puts
-    call puts
+    call puts          
 
     lea print_digits(%rip), %rdi 
     mov $255, %rsi               
     call printf
 
-    lea print_digits(%rip), %rdi
-    mov $1024, %rsi
-    call print_number
 
-    mov $0, %eax        
+    mov -0x8(%rbp), %rbx
+loop_1:
+    lea print_digits(%rip), %rdi
+    mov %rbx, %rsi
+    mov $1024, %rdx
+    call print_number
+    inc %rbx
+    cmp $10, %rbx
+    jle loop_1
+    mov %rbx, %rax
+    mov %rbp, %rsp       
     pop %rbp            
     ret                 
 
