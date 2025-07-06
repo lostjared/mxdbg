9e7ce7d85ca07b3c1cefa6125102022cce66d6cf
Jared Bruni <jaredbruni@gmail.com>
2025-07-06 15:19:36 -0700

    asm test

---
+++ tests/asm/setxy.s
@@ -0,0 +1,25 @@
+.section .data
+ctrl_code:.asciz  "\033[%d;%dH"
+.section .text
+.global setxy
+
+setxy:
+    push %rbp
+    mov %rsp, %rbp
+    sub $16, %rsp
+    mov %rdi, %r12
+    mov %rsi, %r13
+    mov %rdx, %r14
+    lea ctrl_code(%rip), %rdi
+    mov %r12, %rsi
+    mov %r14, %rdx
+    call printf
+    mov $0, %rax
+    mov %rbp, %rsp
+    pop %rbp
+    ret
+
+.section .note.GNU-stack, "",@progbits
+
+
+
---
+++ tests/program.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+void setxy(int y, int x);
+
+int main() {
+    setxy(10, 5);
+    printf("\nHere: \n");
+}
