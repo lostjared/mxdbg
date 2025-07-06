e4252d8f379b0d02ff062a50f149aafcb1795dc4
Jared Bruni <jaredbruni@gmail.com>
2025-07-06 00:22:52 -0700

    tests

--- tests/print_loop.c
+++ tests/print_loop.c
@@ -2,9 +2,11 @@
 
 extern void echo_text(const char *src);
 extern void echo_backwards(const char *src);
+extern void echo_for(const char *src);
 
 int main() {
     echo_text("Hello, World!");
     echo_backwards("Hello, World!");
+    echo_for("Hey world");
     return 0;
 }
