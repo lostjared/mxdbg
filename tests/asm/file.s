dbc240e34807f134c963bbec0296667cbad88bc4
Jared Bruni <jaredbruni@gmail.com>
2025-07-06 00:34:20 -0700

    C library file test with asm

---
+++ tests/asm/file.s
@@ -0,0 +1,33 @@
+.section .data
+file_string: .asciz "Hello, World\n"
+file_name: .asciz "hello.txt"
+file_mode: .asciz "w"
+
+.section  .bss
+.section .text
+.global main
+
+main:
+    push %rbp
+    mov %rsp, %rbp
+    sub $32, %rsp
+    lea file_name(%rip), %rdi
+    lea file_mode(%rip), %rsi
+    call fopen
+    cmp $0, %rax
+    je exit_main
+    mov %rax, -0x8(%rbp)
+    mov %rax, %rdi
+    lea file_string(%rip),%rsi
+    call fprintf
+cleanup:
+    mov -0x8(%rbp), %rdi
+    call fclose
+exit_main:
+    mov $0, %rax
+    mov %rbp, %rsp
+    pop %rbp
+    ret
+
+.section .note.GNU-stack, "",@progbits
+
