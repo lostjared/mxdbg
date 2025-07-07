8945d4752c90fd187d779b9c33f8479a7b8821b9
Jared Bruni <jaredbruni@gmail.com>
2025-07-06 23:12:46 -0700

    uupdated c code

--- tests/trim_com.c
+++ tests/trim_com.c
@@ -1,8 +1,23 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 extern void trim(const char *src);
 
-int main() {
-    trim("Hello # worldd\n#test one\nThis is a test.\n");
-    return 0;
+int main(int argc, char **argv) {
+    if(argc != 2) {
+        printf("Error requires one argument...\n");
+        return EXIT_FAILURE;
+    }
+    FILE *fptr = fopen(argv[1], "r");
+    if(!fptr) {
+        fprintf(stderr, "Error could not open data.txt");
+        return EXIT_FAILURE;
+    }
+    while(!feof(fptr)) {
+        char buffer[256] = {0};
+        fgets(buffer, 255, fptr);
+        trim(buffer);
+    }
+    fclose(fptr);
+    return EXIT_SUCCESS;
 }
