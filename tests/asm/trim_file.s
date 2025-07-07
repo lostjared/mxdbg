990dc2a20549581b69ebfb567091ee307f422340
Jared Bruni <jaredbruni@gmail.com>
2025-07-07 01:52:57 -0700

    trim_file

---
+++ tests/asm/trim_file.s
@@ -0,0 +1,45 @@
+.section .data
+print_char: .asciz "%c"
+
+.section .bss
+.section .text
+.global trim
+
+trim:
+    push %rbp
+    mov %rsp, %rbp
+    sub $32, %rsp
+    mov %rdi, -0x8(%rbp)
+    mov %rsi, %r12
+loop_chars:
+    movb (%r12), %al
+    inc %r12
+    mov -0x8(%rbp), %rdi
+    lea print_char(%rip), %rsi
+    movzx %al, %edx
+    cmpb $0x23, %al
+    je skip_chars
+    test %al, %al
+    jz end_loop
+    call fprintf
+    jmp loop_chars    
+skip_chars:
+    movb (%r12), %al
+    cmpb $0xa, %al
+    inc %r12
+    je loop_chars
+    test %al, %al
+    jz end_loop
+    jmp skip_chars
+end_loop:
+    mov $0, %rax
+    mov %rbp, %rsp
+    pop %rbp
+    ret
+
+.section .note.GNU-stack, "",@progbits
+
+
+
+    
+    
---
+++ tests/trim_file.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+extern void trim(FILE *fptr, const char *src);
+
+int main(int argc, char **argv) {
+    if(argc != 3) {
+        printf("Error requires two arguments...\n");
+        return EXIT_FAILURE;
+    }
+    FILE *fptr = fopen(argv[1], "r");
+    if(!fptr) {
+        fprintf(stderr, "Error could not open %s", argv[1]);
+        return EXIT_FAILURE;
+    }
+    FILE *fout = fopen(argv[2], "w");
+    if(!fout) {
+        fprintf(stderr, "Error opening file: %s\n", argv[2]);
+        fclose(fptr);
+        return EXIT_FAILURE;
+    }
+    while(!feof(fptr)) {
+        char buffer[256] = {0};
+        fgets(buffer, 255, fptr);
+        trim(fout, buffer);
+    }
+    fclose(fptr);
+    fclose(fout);
+    return EXIT_SUCCESS;
+}
